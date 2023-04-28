a86f5d73486d8a8838193e94e16f0f6545ad2db1
Dan Bowen <danbowen2677@gmail.com>
2023-04-28 10:58:25 -0400

    created the initial model structure

---
+++ models/BlogPost.js
@@ -0,0 +1,42 @@
+// By destructuring sequelize we can only pull out the things we need: in this case, the model and the datatypes so we can define the BlogPost model
+const { Model, DataTypes} = require('sequelize')
+
+// connect to the database using sequelize.
+const sequelize = require('../config/connection');
+
+// The class extends Model is one of the ways in sequelize that models can be defined. This line of code allows us to use the init method a few lines later in the code and to define all the model attributes. 
+class BlogPost extends Model{}
+
+BlogPost.init(
+    {
+        // defining all the model attributes
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,   
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogpost',
+    }
+)
+
+module.exports = BlogPost
---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+// By destructuring sequelize we can only pull out the things we need: in this case, the model and the datatypes so we can define the Comment model
+const { Model, DataTypes} = require('sequelize')
+
+// connect to the database using sequelize.
+const sequelize = require('../config/connection');
+
+// The class extends Model is one of the ways in sequelize that models can be defined. This line of code allows us to use the init method a few lines later in the code and to define all the model attributes. 
+class Comment extends Model{}
+
+Comment.init(
+    {
+        // defining all the model attributes
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        blogpost_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            reference: {
+                model: 'blogpost',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            reference: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+)   
+
+module.exports = Comment
